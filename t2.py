f7b6e9dce775439faa1c4393a4eb0ce890d996f6
kgforsure <kgforsure@www.kgforsure.horse>
2026-07-14 09:07:28 +0800

    add some note

--- t2.py
+++ t2.py
@@ -3,13 +3,14 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
+
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, confusion_matrix
 
 # 1. 讀取與處理
 df = pd.read_csv('/home/kgforsure/Downloads/world cup datasets/player_stats.csv')
 
-# 假設你已經選擇好了要分類的隊伍 (例如前兩大)
+# 處理資料，根據需要的特徵分成X, y
 top_teams = df['team'].value_counts().nlargest(2).index
 df_filtered = df[df['team'].isin(top_teams)].copy()
 tech_cols = ['pressures', 'progressive_passes', 'progressive_carries']
@@ -19,11 +20,18 @@ for col in tech_cols:
 X = df_filtered.drop(columns=['team', 'player_id', 'player', 'match_id', 'position_group','minutes'])
 y = df_filtered['team']
 
-# 自動偵測數值欄位 (過濾掉文字型別)
+# 資料清洗
 numeric_features = X.select_dtypes(include=['int64', 'float64']).columns
 
 # 2. 建立 Pipeline
 # 隨機森林其實不需要 StandardScaler，但保留它無妨，或可選擇註解掉
+# Pipeline(steps=[
+#     ('步驟名稱1', 轉換器物件),
+#     ('步驟名稱2', 轉換器物件),
+#     ('最後的工站', 模型物件)
+# ])
+# 轉換器物件要可以有要有 fit 和 transform 方法
+# 估計器Estimator, 要有 fit 方法（通常就是你的模型
 model = Pipeline(steps=[
     ('preprocessor', ColumnTransformer(
         transformers=[
@@ -43,6 +51,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 # 4. 訓練
+# 訓練後的model就是黑盒子，就直接拿來用
 model.fit(X_train, y_train)
 
 # 5. 評估結果
@@ -50,6 +59,7 @@ print(f"訓練集準確率: {model.score(X_train, y_train):.2f}")
 print(f"測試集準確率: {model.score(X_test, y_test):.2f}")
 print("\n詳細報告:\n", classification_report(y_test, y_pred=model.predict(X_test)))
 # model.predict_proba()
+
 # 6. 查看特徵重要性 (這是隨機森林的靈魂！)
 importances = model.named_steps['classifier'].feature_importances_
 feat_imp = pd.Series(importances, index=numeric_features).sort_values(ascending=False)
